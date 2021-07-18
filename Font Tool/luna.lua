ed38659d545ef18fbeed252b50211472ff4c0409
rockythechao <mack151.rockythechao@yahoo.com>
2021-07-18 14:30:45 -0500

    widget integration + assorted polish
    - The new widgets are now fully integrated and the old buttons have been removed
    - Tidied up the overall layout a bit
    - Made the preview scrollbars take up existing preview panel space and adjust their width/height based on whether one or both are present
    - Made a reload font button to quickly preview changes to ini files
    - Added a (currently commented out) mode listbox with Preview, Defaults, and Character modes for potential future editing functionality

--- Font Tool/luna.lua
+++ Font Tool/luna.lua
@@ -22,16 +22,26 @@ function wrapInt(current, minVal, maxVal)
 	-- return wrapped
 end
 
-local fontUI = miniui.ListBox{x = 16, y = 390, name = 'Font: ', list = {'a', 'b', 'c'}, lineEditSize = vector(256, 32)}
-local scaleUI = miniui.SpinBox{min = 0, max = 10, int = 0.5, x = 16, y = 180, name = "Scale: ", default = 1} -- Can we get a scaleX and scaleY option? sometimes I use tall text and it would be nice to see what it looks like
-local waveUI = miniui.SpinBox{min = -10, max = 10, int = 0.5, x = 16, y = 250, name = "Wave: ", default = 0}
-local colorUI = miniui.ListBox{x = 16, y = 320, name = 'Color: ', list = {'White', 'Black', 'Gray', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Rainbow'}}
+local fontUI = miniui.ListBox{x = 16, y = 20, name = 'Font: ', list = {'a', 'b', 'c'}, lineEditSize = vector(256, 32)}
+--local modeUI = miniui.ListBox{x = 16, y = 120, name = 'Mode: ', list = {'Preview', 'Defaults', 'Character'}, lineEditSize = vector(152, 32)}
+
+
+-- Global mode
+
+-- Preview mode
+local scaleUI = miniui.SpinBox{min = 0.5, max = 10, int = 0.5, x = 16, y = 316, name = "Scale: ", default = 1, lineEditSize = vector(152, 32)}
+local scaleXUI = miniui.SpinBox{min = 0.5, max = 5, int = 0.5, x = 16, y = 386, name = "Scale X: ", default = 1, lineEditSize = vector(40, 32)}
+local scaleYUI = miniui.SpinBox{min = 0.5, max = 5, int = 0.5, x = 128, y = 386, name = "Scale Y: ", default = 1, lineEditSize = vector(40, 32)}
+local waveUI = miniui.SpinBox{min = -10, max = 10, int = 0.5, x = 16, y = 456, name = "Wave: ", default = 0, lineEditSize = vector(40, 32)}
+local glitchUI = miniui.SpinBox{min = 0, max = 1, int = 0.1, x = 128, y = 456, name = "Glitch: ", default = 0, lineEditSize = vector(40, 32)}
+local colorUI = miniui.ListBox{x = 16, y = 526, name = 'Color: ', list = {'White', 'Black', 'Gray', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Rainbow'}, lineEditSize = vector(152, 32)}
 
 local fontIndex = 1
 local fontList = {}
 local fontMap = {}
+local fontPaths = {}
 local localCount = 1
-local defaultTestString = [[The quick brown fox<br>jumps over the lazy dog<br>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br>abcdefghijklmnopqrstuvwxyz<br>1234567890.,!?:;'"&%()[]{}<br>*+=-_<br><wave 3>wavy text</wave> <glitch 1>glitched text</glitch> <color rainbow>colors</color>]]
+local defaultTestString = [[The quick brown fox<br>jumps over the lazy dog<br>ABCDEFGHIJKLMNOPQRSTUVWXYZ<br>abcdefghijklmnopqrstuvwxyz<br>1234567890.,!?:;'"&%()[]{}<br>*+=-_]]
 
 
 
@@ -46,16 +56,17 @@ local display = {
 
     -- Control vars
     index = 1,
-    scaleIndex = 9,
 
     bounds = {
-        top = 100,
-        bottom = 550,
-        left = 200,
-        right = 750
+        top = 150,
+        bottom = 582,
+        left = 250,
+        right = 782
     },
 
     scale = 2,
+    xscale = 1,
+    yscale = 1,
     scrollPos = vector(0,0),
     scrollMax = vector(1000,1000),
     testString = defaultTestString,
@@ -92,75 +103,53 @@ local buttonGfx = {
     right = Graphics.loadImageResolved("button_right.png"),
     up = Graphics.loadImageResolved("button_up.png"),
     down = Graphics.loadImageResolved("button_down.png"),
-    reset = Graphics.loadImageResolved("button_reset.png"),
+    reset = Graphics.loadImageResolved("button_reset.png")
 }
 local buttons = {
 
-    -- Font change buttons
-    fontLeft = {
-        x=32,y=56, img=buttonGfx.left,
-        onPress = function(self)
-            display.index = wrapInt(display.index-1, 1, #fontList)
-            display.layoutDirty = true
-        end
-    },
-    fontRight = {
-        x=64,y=56, img=buttonGfx.right,
-        onPress = function(self)
-            display.index = wrapInt(display.index+1, 1, #fontList)
-            display.layoutDirty = true
-        end,
-    },
-
-
-    -- Scale change buttons
-    scaleLeft = {
-        x=32,y=136, img=buttonGfx.left,
-        onPress = function(self)
-            display.scaleIndex = display.scaleIndex-1
-            display.scale = scales[display.scaleIndex]
-            display.layoutDirty = true
-        end
-    },
-    scaleRight = {
-        x=64,y=136, img=buttonGfx.right,
-        onPress = function(self)
-            display.scaleIndex = display.scaleIndex+1
-            display.scale = scales[display.scaleIndex]
-            display.layoutDirty = true
-        end
-    },
-
-
     -- Display scroll buttons
     displayLeft = {
-        x=display.bounds.left+16, y=display.bounds.bottom+16,
+        x=display.bounds.left+16, y=display.bounds.bottom-16,
         img = buttonGfx.left,
         onHold = function(self)
             display.scrollPos.x = math.max(0, display.scrollPos.x-10)
         end
     },
     displayRight = {
-        x=display.bounds.right-16, y=display.bounds.bottom+16,
+        x=display.bounds.right-16, y=display.bounds.bottom-16,
         img = buttonGfx.right,
         onHold = function(self)
             display.scrollPos.x = math.min(display.scrollMax.x, display.scrollPos.x+10)
         end
     },
     displayUp = {
-        x=display.bounds.right+16, y=display.bounds.top+16,
+        x=display.bounds.right-16, y=display.bounds.top+16,
         img = buttonGfx.up,
         onHold = function(self)
             display.scrollPos.y = math.max(0, display.scrollPos.y-10)
         end
     },
     displayDown = {
-        x=display.bounds.right+16, y=display.bounds.bottom-16,
+        x=display.bounds.right-16, y=display.bounds.bottom-16,
         img = buttonGfx.down,
         onHold = function(self)
             display.scrollPos.y = math.min(display.scrollMax.y, display.scrollPos.y+10)
         end
     },
+
+
+    -- Reload font
+    ---[[
+    reloadFont = {
+        x=fontUI.x + fontUI.lineEditSize.x + 64 + 24, y=fontUI.y + 40,
+        img = buttonGfx.reset,
+        onPress = function(self)
+            local _,fontName = display:getCurrentFont()
+            fontMap[fontName] = textplus.loadFont(fontPaths[fontName])
+            display.layoutDirty = true
+        end
+    },
+    --]]
 
 
     -- Reset test string
@@ -181,6 +170,7 @@ local buttonScroll = {
 
 
 local labels = {
+    --[[
     fontname = {
         x = 20,y=20,
         text = ""
@@ -189,6 +179,7 @@ local labels = {
         x=20, y=96,
         text = ""
     },
+    --]]
     testString = {
         x = 2,
         y = 4,
@@ -207,7 +198,7 @@ fontviewer = {}
 function onStart()
     Graphics.activateHud(false)
 
-    display.scaleIndex = table.ifind(scales, display.scale)
+    display.scale = 2
 
 
     -- Process buttons
@@ -233,7 +224,7 @@ function onStart()
 
 
     -- Scroll buttons
-    for  _,k in ipairs{"fontLeft","fontRight","scaleLeft","scaleRight"}  do
+    for  _,k in ipairs{--[["fontLeft","fontRight","scaleLeft","scaleRight"]]}  do
         local v = buttons[k]
         v.onScroll = function(self)
             self:onPress()
@@ -244,17 +235,20 @@ function onStart()
     -- Load fonts
     local fontMaps = {}
     local fontLists = {}
+    local fontPathMaps = {}
+
     for  k,v in ipairs{
         {files = Misc.listLocalFiles("fonts"),                 root="fonts/"},
         {files = Misc.listFiles(     "scripts/textplus/font"), root="textplus/font/"}
     }  do
 
         fontMaps[k] = {}
+        fontPathMaps[k] = {}
         for  _,v2 in ipairs(v.files)  do
             local str = string.gsub(v2, "([^%.]+)(%.)(%w+)", function(name,dot,extension)
                 if  extension == "ini"  then
                     --Misc.dialog(name, dot, extension)
-
+                    fontPathMaps[k][name] = v.root..v2
                     fontMaps[k][name] = textplus.loadFont(v.root..v2)
                 end
             end)
@@ -263,6 +257,7 @@ function onStart()
         table.sort(fontLists[k])
     end
     fontMap = table.join(fontMaps[1], fontMaps[2])
+    fontPaths = table.join(fontPathMaps[1], fontPathMaps[2])
     fontList = table.append(fontLists[1], fontLists[2])
     localCount = #fontLists[1]
 
@@ -278,7 +273,6 @@ end
 
 
 function onDraw()
-	display.scale = scaleUI.value -- im not sure how to do the thing to do the thing
 
     -- exposed table integration
     if  fontviewer.text ~= nil  and  fontviewer.text ~= display.testString  then
@@ -287,9 +281,33 @@ function onDraw()
     display.testString = fontviewer.text  or  display.testString
 
 
+    -- Modes
+    for  k,v in ipairs{scaleUI, scaleXUI, scaleYUI, waveUI, glitchUI, colorUI}  do
+        v.active = true --modeUI.value == "Preview"
+    end
+
+
+    -- New widget integration
+    if  display.index ~= fontUI.value
+    or  display.scale ~= scaleUI.value
+    or  display.xscale ~= scaleXUI.value
+    or  display.yscale ~= scaleYUI.value
+    or  display.glitch ~= glitchUI.value
+    or  display.color ~= colorUI.value
+    then
+        display.layoutDirty = true
+    end
+    fontUI.list = fontList
+    display.index = fontUI.index
+    display.scale = scaleUI.value
+    display.xscale = scaleXUI.value
+    display.yscale = scaleYUI.value
+    display.glitch = glitchUI.value
+    display.color = colorUI.value
+
+
     -- Get the current font
     local currentFont, fontName = display:getCurrentFont()
-
 
     -- Update the test string plaintext
     if  display.testStringDirty  then
@@ -304,11 +322,10 @@ function onDraw()
         display.layoutDirty = true
     end
 
-
     -- Text entry field interactivity
     testStringField.buffer:clear(100)
     local stringFieldPos = vector(display.bounds.left+48, display.bounds.top-38)
-    local stringFieldSize = vector(525, 28)
+    local stringFieldSize = vector(display.bounds.w-64, 28)
 
     local stringFieldAdd = ""
 
@@ -332,8 +349,8 @@ function onDraw()
         basegameFontAdd = "(BASEGAME) "
     end
 
-    labels.fontname.text = "Font: "..basegameFontAdd..fontName
-    labels.scale.text = "Scale: "..tostring(display.scale)
+    --labels.fontname.text = "Font: "..basegameFontAdd..fontName
+    --labels.scale.text = "Scale: "..tostring(display.scale)
     labels.testString.text = display.testStringPlaintext..stringFieldAdd
 
     for  _,k in ipairs(labelList)  do
@@ -378,16 +395,20 @@ function onDraw()
         buttonScroll.startTick = lunatime.tick()
     end
 
-    buttons.scaleLeft.enabled = display.scaleIndex > 1
-    buttons.scaleRight.enabled = display.scaleIndex < #scales
-
     local frame
     local scrollTick
     for  _,k in ipairs(buttonList)  do
         v = buttons[k]
 
-        v.sprite.width = v.w  or  v.sprite.width
+        v.sprite.x =      v.x  or  v.sprite.x
+        v.sprite.y =      v.y  or  v.sprite.y
+        v.sprite.width =  v.w  or  v.sprite.width
         v.sprite.height = v.h  or  v.sprite.height
+
+        v.x1 = v.sprite.x - v.w
+        v.x2 = v.sprite.x + v.w
+        v.y1 = v.sprite.y - v.h
+        v.y2 = v.sprite.y + v.h
 
         -- Render if not flagged as invisible
         if  v.visible ~= false  then
@@ -438,19 +459,28 @@ function onDraw()
 
 
     -- Update layout, scroll limits, and buffer
+    local displaySize = vector(display.bounds.w, display.bounds.h)
+
     if  display.layoutDirty  then
         display.layoutDirty = false
-        local parsed = textplus.parse("<wave "..waveUI.value..">"..display.testString.."</wave>", {font=currentFont, xscale=display.scale, yscale=display.scale})
+        local parsed = textplus.parse("<color "..string.lower(colorUI.value).."><wave "..waveUI.value.."><glitch "..glitchUI.value..">"..display.testString.."</glitch></wave></color>", {font=currentFont, xscale=display.scale*display.xscale, yscale=display.scale*display.yscale})
         display.layout = textplus.layout(parsed)
 
         display.scrollMax = vector(
-            math.max(0, display.layout.width+20-display.bounds.w),
-            math.max(0, display.layout.height+20-display.bounds.h)
+            math.max(0, display.layout.width + 20 - display.bounds.w),
+            math.max(0, display.layout.height + 20 - display.bounds.h)
         )
         display.scrollPos = vector(
             math.min(display.scrollMax.x, display.scrollPos.x),
             math.min(display.scrollMax.y, display.scrollPos.y)
         )
+
+        if  display.scrollMax.x > 0  then
+            displaySize.y = displaySize.y-32
+        end
+        if  display.scrollMax.y > 0  then
+            displaySize.x = displaySize.x-32
+        end
 
         display.buffer = CaptureBuffer(display.layout.width+20,display.layout.height+20)
 
@@ -459,6 +489,8 @@ function onDraw()
         buttons.displayUp.visible = display.scrollMax.y > 0
         buttons.displayDown.visible = display.scrollMax.y > 0
     end
+    buttons.displayRight.x = display.bounds.left + displaySize.x - 16
+    buttons.displayDown.y = display.bounds.top + displaySize.y - 16
 
 
     -- Scrollbars
@@ -469,21 +501,21 @@ function onDraw()
 
         -- Bar
         Graphics.drawBox{
-            x=display.bounds.left, y=display.bounds.bottom,
-            w=display.bounds.w, h=32,
+            x=display.bounds.left, y=display.bounds.bottom-32,
+            w=displaySize.x, h=32,
             color = Color.darkgray,
             priority=-3
         }
         Graphics.drawBox{
-            x=display.bounds.left+2, y=display.bounds.bottom+2,
-            w=display.bounds.w-4, h=28,
+            x=display.bounds.left+2, y=display.bounds.bottom-30,
+            w=displaySize.x-4, h=28,
             color = Color.gray,
             priority=-2
         }
 
         -- Box
         Graphics.drawBox{
-            x=display.bounds.left+32 + math.lerp(0,display.bounds.w-92, scrollPercent.x), y=display.bounds.bottom+2,
+            x=display.bounds.left+32 + math.lerp(0,displaySize.x-92, scrollPercent.x), y=display.bounds.bottom-30,
             w=28, h=28,
             color = Color.lightgray,
             priority=-1
@@ -495,21 +527,21 @@ function onDraw()
 
         -- Bar
         Graphics.drawBox{
-            x=display.bounds.right, y=display.bounds.top,
-            w=32, h=display.bounds.h,
+            x=display.bounds.right-32, y=display.bounds.top,
+            w=32, h=displaySize.y,
             color = Color.darkgray,
             priority=-3
         }
         Graphics.drawBox{
-            x=display.bounds.right+2, y=display.bounds.top+2,
-            w=28, h=display.bounds.h-4,
+            x=display.bounds.right-30, y=display.bounds.top+2,
+            w=28, h=displaySize.y-4,
             color = Color.gray,
             priority=-2
         }
 
         -- Box
         Graphics.drawBox{
-            x=display.bounds.right+2, y=display.bounds.top+32 + math.lerp(0,display.bounds.h-92, scrollPercent.y),
+            x=display.bounds.right-30, y=display.bounds.top+32 + math.lerp(0,displaySize.y-92, scrollPercent.y),
             w=28, h=28,
             color = Color.lightgray,
             priority=-1
@@ -521,22 +553,22 @@ function onDraw()
     if  cursor.left == KEYS_DOWN  then
         -- Horz drag
         if   cursor.x > display.bounds.left+32
-        and  cursor.x < display.bounds.right-32
-        and  cursor.y > display.bounds.bottom
-        and  cursor.y < display.bounds.bottom+32
+        and  cursor.x < display.bounds.left + displaySize.x - 32
+        and  cursor.y > display.bounds.bottom-32
+        and  cursor.y < display.bounds.bottom
         and  display.scrollMax.x > 0
         then
-            local percent = math.invlerp(display.bounds.left+48,display.bounds.right-48, cursor.x)
+            local percent = math.invlerp(display.bounds.left+48,display.bounds.left + displaySize.x-48, cursor.x)
             display.scrollPos.x = display.scrollMax.x*math.clamp(percent,0,1)
         end
         -- Vert drag
-        if   cursor.x > display.bounds.right
-        and  cursor.x < display.bounds.right+32
+        if   cursor.x > display.bounds.right-32
+        and  cursor.x < display.bounds.right
         and  cursor.y > display.bounds.top+32
-        and  cursor.y < display.bounds.bottom-32
+        and  cursor.y < display.bounds.top + displaySize.y - 32
         and  display.scrollMax.y > 0
         then
-            local percent = math.invlerp(display.bounds.top+48,display.bounds.bottom-48, cursor.y)
+            local percent = math.invlerp(display.bounds.top+48,display.bounds.top + displaySize.y-48, cursor.y)
             display.scrollPos.y = display.scrollMax.y*math.clamp(percent,0,1)
         end
     end
@@ -554,12 +586,14 @@ function onDraw()
 
     Graphics.drawBox{
         x=display.bounds.left, y=display.bounds.top,
-        w=display.bounds.w, h=display.bounds.h,
+        w=displaySize.x,
+        h=displaySize.y,
         color = Color.darkgray
     }
     Graphics.drawBox{
         x=display.bounds.left+2, y=display.bounds.top+2,
-        w=display.bounds.w-4, h=display.bounds.h-4,
+        w=displaySize.x-4,
+        h=displaySize.y-4,
         sourceWidth = display.bounds.w-4,
         sourceHeight = display.bounds.h-4,
         sourceX = display.scrollPos.x,
